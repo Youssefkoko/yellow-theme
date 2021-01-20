ce2578cf0ce6ff668051c804e9267899781d13f2
Sonja <youssefkoko01@gmail.com>
2021-01-20 23:52:36 +0100

    setup Html elements and wp tags

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -12,9 +12,24 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
+?>
+<div class="home-hero-banner-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <div class="skip-bin-info-holder">
+          <div class="title">
+            <i class="fa fa-arrow-circle-up"></i>
+            Skip Bin Instant Quote
+          </div>
+          <div class="desc">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, deserunt.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-
-
-
-
+<?php
 get_footer();
