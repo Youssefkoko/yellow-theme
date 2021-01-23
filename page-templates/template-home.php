1edc396f4592879c25950c8f76470600111e2288
Sonja <youssefkoko01@gmail.com>
2021-01-24 00:00:18 +0100

    change the name of the file

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -73,6 +73,95 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!-- End of Hero Banner  -->
 <!-- Products  -->
 <section class="home-products-info">
+  <div class="home-product-holder-1">
+    <div class="container">
+      <div class="home-special-offer d-flex flex-md-row flex-column align-items-center">
+        <div class="discount-circle d-flex flex-column justify-content-center align-items-center">
+          <div class="save uppercase">
+            Save
+          </div>
+          <div class="percent">20%</div>
+        </div>
+        <div class="entry-content d-flex flex-column align-items-center justify-content-between ">
+          <h1 class="title">15 Days Storm</h1>
+          <div class="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur adipisci facilis
+            quibusdam quos cumque quis laborum nostrum vel animi amet.</div>
+          <a href="#" class="btn btn-lg btn-outline-primary" type="button" role="button">Find Out More Info</a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="home-product-holder-2">
+    <div class="container">
+      <h2 class="mb-5 text-center">
+        <i class="fa fa-arrow-circle-down d-none d-sm-inline text-secondary mr-2"></i>
+        <span class="d-block d-sm-inline-block mb-3 mb-sm-0">Select The Right Skip Bin For
+          your project</span>
+        <i class="fa fa-arrow-circle-left d-sm-none d-inline  text-secondary mr-2"></i>
+        <i class="fa fa-arrow-circle-right d-sm-none d-inline text-secondary mr-2"></i>
+      </h2>
+      <div class="home-products  bg-white text-secondary">
+        <div class="home-product p-5">
+          <div class="row">
+            <div class="col-xl-4 col-md-6">
+              <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/6m3-skip-bin.png"
+                alt="Bin For Sale">
+            </div>
+            <div class="col-xl-4 col-md-6 order-md-3">
+              <h3 class="title">Cubic Metri Skip</h3>
+              <ul class="list-unstyled">
+                <li>
+                  Clean up and HouseHold Garden
+                </li>
+                <li>
+                  Heavy Loads (dirt, concrete, bricks or rubble)
+                </li>
+                <li>
+                  Ideal for narrow alleys or small streets
+                </li>
+                <li>
+                  Building Sites
+                </li>
+              </ul>
+            </div>
+            <div class="col-xl-4 col-md-6 order-lg-3 mx-auto">
+              <div class="home-product-circle">
+                <div class="row justify-content-end">
+                  <div class="col-lg-6 col-md-6 col-sm-6 mb-3">
+                    <div class="circle mx-auto d-flex flex-column align-items-center justify-content-center">
+                      <div class="content-holder">
+                        Aproximity x 4 Trailers
+                      </div>
+                      <div class="icon-img">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/home-trailer.svg"
+                          alt="">
+                      </div>
+                    </div>
+                    <!-- End of Col -->
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-6 mb-3">
+                    <div class="circle mx-auto d-flex flex-column align-items-center justify-content-center">
+                      <div class="content-holder">
+                        Aproximity x
+                        16 wheele Bin
+                      </div>
+                      <div class="icon-img">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/bin.svg" alt="">
+                      </div>
+                    </div>
+                    <!-- End of Col  -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- End of Row  -->
+          <div class="abs-holder-button text-uppercase ">Get Quots For Skip Bin</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 <!-- End of Products -->
 
